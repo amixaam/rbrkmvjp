7c2ab1451904d2088fcc0e63437f4125b113a5b0
amixam <robertobrinko@gmail.com>
2024-01-09 13:31:18 +0200

    routes

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-    return (
-        <div className='flex flex-col items-center container mx-auto max-w-2xl shadow  rounded border-blue-400 border p-4 my-5'>
-            <span className='text-xl '>🩵 Thank you for cloning clean version of me 🩵</span>
-        </div>
-    )
-}
