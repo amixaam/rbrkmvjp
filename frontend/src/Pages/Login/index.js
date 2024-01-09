918f4f772aaac2055651a990f2d1d4d954cc2310
eskruts <113087138+eskruts@users.noreply.github.com>
2024-01-09 13:50:42 +0200

    img

---
+++ frontend/src/Pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function Login() {
+    return( 
+        <div className="login-container">
+
+        </div>
+    );
+}
+
+export default Login;
