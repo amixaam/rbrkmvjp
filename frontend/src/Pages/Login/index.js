f7cb7410e630032fbf16cc2e94c9f08aeed0f97c
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2024-01-10 11:29:00 +0200

    asd

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -8,11 +8,17 @@ function Login() {
                 <h1 className="title">RBRKMVJP</h1>
                 <div className="login-layout">
                     <div className="login-input">
-                        <h2 className="input-text">Username</h2>
+                        <div className="input-row">
+                            <h2 className="input-text">Username</h2>
+                            <h3 className="error-text">Error message</h3>
+                        </div>
                         <input type="text" name="name"/> 
                     </div>   
                     <div className="login-input">
-                        <h2 className="input-text">Username</h2>
+                        <div className="input-row">
+                            <h2 className="input-text">Username</h2>
+                            <h3 className="error-text">Error message</h3>
+                        </div>
                         <input type="text" name="name"/> 
                     </div>    
                 </div>
