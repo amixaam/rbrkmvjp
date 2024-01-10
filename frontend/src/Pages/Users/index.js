33e3fdc6b0af828e54dc2f1cede102881f436bdb
Lxgends <marcitis24@gmail.com>
2024-01-10 11:22:11 +0200

    RRMJ-7
    Bumbas

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./User.css";
+import LeftArrow from "../../images/left_arrow.svg"
+import RightArrow from "../../images/right_arrow.svg"
 
 function Users() {
-    return <div>Users</div>;
+    return <>
+        <div className="main-user-container">
+            <div className="user-container">
+                <div className="title">
+
+                </div>
+                <div className="userList">
+                    <div className="specific-user">
+
+                    </div>
+                </div>
+                <div className="buttons">
+                    <div className="atskaites">
+                        <button>Atskaite</button>
+                    </div>
+                    <div className="page-swappers">
+                        <button><img src={LeftArrow} alt="Previous Button"/></button>
+                        <button><img src={RightArrow} alt="Next Button"/></button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </>
 }
 
 export default Users;
