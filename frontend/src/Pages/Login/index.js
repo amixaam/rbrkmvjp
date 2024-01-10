71dafb9e4778ec2a73e790ab34ceed5c915f178e
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2024-01-10 10:39:25 +0200

    Asd

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -4,7 +4,22 @@ import "./index.css";
 function Login() {
     return( 
         <div className="login-container">
-
+            <div className="login-box">
+                <h1 className="title">RBRKMVJP</h1>
+                <div className="login-layout">
+                    <div className="login-input">
+                        <h2 className="input-text">Username</h2>
+                        <input type="text" name="name"/> 
+                    </div>   
+                    <div className="login-input">
+                        <h2 className="input-text">Username</h2>
+                        <input type="text" name="name"/> 
+                    </div>    
+                </div>
+                <button className="login-button">
+                    <h1 className="text">Log in</h1>
+                </button>
+            </div>
         </div>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Login from "./Pages/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/Login" element={<Login />} />
+        </Routes>
+    );
+};
+
+const root = createRoot(document.getElementById("root"));
+
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
-
