6e75c152323975ef1c09b8d35ba8a22df234cf17
amixam <robertobrinko@gmail.com>
2024-01-12 14:27:44 +0200

    STYLE - BACKGROUND for all views + ROOT colors

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./index.css";
+import Background from "../../Reusable/Background";
 
 function Login() {
-    return( 
+    return (
         <div className="login-container">
             <div className="login-box">
                 <h1 className="title">RBRKMVJP</h1>
@@ -12,20 +13,21 @@ function Login() {
                             <h2 className="input-text">Username</h2>
                             <h3 className="error-text">Error message</h3>
                         </div>
-                        <input type="text" name="name"/> 
-                    </div>   
+                        <input type="text" name="name" />
+                    </div>
                     <div className="login-input">
                         <div className="input-row">
                             <h2 className="input-text">Username</h2>
                             <h3 className="error-text">Error message</h3>
                         </div>
-                        <input type="text" name="name"/> 
-                    </div>    
+                        <input type="text" name="name" />
+                    </div>
                 </div>
                 <button className="login-button">
                     <h1 className="text">Log in</h1>
                 </button>
             </div>
+            <Background />
         </div>
     );
 }
---
+++ frontend/src/Reusable/Background.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Background.css";
+
+// Ielikt main containerā
+function Background() {
+    return <div className="background-container"></div>;
+}
+
+export default Background;
