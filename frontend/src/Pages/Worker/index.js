1804e5df5a73d814829a4fc74a1dea32d735588c
Lxgends <marcitis24@gmail.com>
2024-01-16 13:38:05 +0200

    Fixed issues that didn't push

---
+++ frontend/src/Pages/Worker/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function Worker() {
+    return (
+        <>
+            <div className="worker-main-container">
+
+            </div>
+        </>
+    );
+}
+
+export default Worker;
