472e1901c68c943ee6fd7ac60699bcb1423d3041
Lxgends <marcitis24@gmail.com>
2024-01-16 13:36:56 +0200

    Fixed users
    Didn't push correctly

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import "./User.css";
+import "./index.css";
+import "../../text.css";
+import "../../Reusable/Background.css"
 import LeftArrow from "../../images/left_arrow.svg"
 import RightArrow from "../../images/right_arrow.svg"
-import "../../text.css";
 
 function Users() {
     return <>
         <div className="main-user-container">
+            <div className="background-container"></div>
             <div className="user-container">
                 <div className="title">
                     <h1 className="text-primary">Admin interface</h1>
@@ -15,22 +17,33 @@ function Users() {
                     <p className="text-secondary">Users</p>
                     <div className="specific-user-titles">
                         <div className="id-title">
-                            <p>ID</p>
+                            <p className="small-text">ID</p>
                         </div>
                         <div className="role-title">
-                            <p>Role</p>
+                            <p className="small-text">Role</p>
                         </div>
                         <div className="other-title">
-                            <p>Other</p>
+                            <p className="small-text">Other</p>
                         </div>
                     </div>
                     <div className="specific-user">
-
+                        <div className="id-container">
+                            <p>x</p>
+                        </div>
+                        <div className="role-container">
+                            <p>x</p>
+                        </div>
+                        <div className="other-container">
+                            <p>x</p>
+                        </div>
+                        <div className="edit-container">
+                            <button className="edit-buttons"><p className="text-buttons">Edit</p></button>
+                        </div>
                     </div>
                 </div>
                 <div className="buttons">
                     <div className="creating-stuff">
-                    <button className="create-buttons"><p className="text-buttons">Add User</p></button>
+                        <button className="create-buttons"><p className="text-buttons">Add User</p></button>
                         <button className="create-buttons"><p className="text-buttons">Create Report</p></button>
                     </div>
                     <div className="page-swappers">
