1755c94d98b44df726238f1e3a36311ca6e9bb67
Lxgends <marcitis24@gmail.com>
2024-01-16 14:46:12 +0200

    Fixed 3x

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./User.css";
 import "../../Reusable/text.css";
 import "../../Reusable/Background.css"
 import LeftArrow from "../../images/left_arrow.svg"
 import RightArrow from "../../images/right_arrow.svg"
-import React, { useState, useRef, useEffect } from "react";
 
 function Users() {
   const [isPopupOpen, setPopupOpen] = useState(false);
