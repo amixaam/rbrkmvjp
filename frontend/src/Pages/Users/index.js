b31d8152aef1b8560f68fcb87bfe0e1fb6382874
Lxgends <marcitis24@gmail.com>
2024-01-16 13:24:17 +0200

    Completed Sprint
    Frontend is finished

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -1,48 +1,62 @@
 import React from "react";
 import "./User.css";
+import "../../index.css";
+import "../../text.css";
+import "../../Reusable/Background.css"
 import LeftArrow from "../../images/left_arrow.svg"
 import RightArrow from "../../images/right_arrow.svg"
-import "../../text.css";
+
 
 function Users() {
     return <>
         <div className="main-user-container">
-            <div className="user-container">
-                <div className="title">
-                    <h1 className="text-primary">Admin interface</h1>
-                </div>
-                <div className="user-list">
-                    <p className="text-secondary">Users</p>
-                    <div className="specific-user-titles">
-                        <div className="id-title">
-                            <p>ID</p>
+            <div className="background-container"></div>
+                <div className="user-container">
+                    <div className="title">
+                        <h1 className="text-primary">Admin interface</h1>
+                    </div>
+                    <div className="user-list">
+                        <p className="text-secondary">Users</p>
+                        <div className="specific-user-titles">
+                            <div className="id-title">
+                                <p className="small-text">ID</p>
+                            </div>
+                            <div className="role-title">
+                                <p className="small-text">Role</p>
+                            </div>
+                            <div className="other-title">
+                                <p className="small-text">Other</p>
+                            </div>
                         </div>
-                        <div className="role-title">
-                            <p>Role</p>
-                        </div>
-                        <div className="other-title">
-                            <p>Other</p>
+                        <div className="specific-user">
+                            <div className="id-container">
+                                <p>x</p>
+                            </div>
+                            <div className="role-container">
+                                <p>x</p>
+                            </div>
+                            <div className="other-container">
+                                <p>x</p>
+                            </div>
+                            <div className="edit-container">
+                                <button className="edit-buttons"><p className="text-buttons">Edit</p></button>
+                            </div>
                         </div>
                     </div>
-                    <div className="specific-user">
-
+                    <div className="buttons">
+                        <div className="creating-stuff">
+                            <button className="create-buttons"><p className="text-buttons">Add User</p></button>
+                            <button className="create-buttons"><p className="text-buttons">Create Report</p></button>
+                        </div>
+                        <div className="page-swappers">
+                            <button className="swap-buttons"><img src={LeftArrow} alt="Previous Button"/></button>
+                            <button className="swap-buttons"><img src={RightArrow} alt="Next Button"/></button>
+                        </div>
                     </div>
                 </div>
-                <div className="buttons">
-                    <div className="creating-stuff">
-                    <button className="create-buttons"><p className="text-buttons">Add User</p></button>
-                        <button className="create-buttons"><p className="text-buttons">Create Report</p></button>
-                    </div>
-                    <div className="page-swappers">
-                        <button className="swap-buttons"><img src={LeftArrow} alt="Previous Button"/></button>
-                        <button className="swap-buttons"><img src={RightArrow} alt="Next Button"/></button>
-                    </div>
-
-                </div>
-
             </div>
-        </div>
     </>
+
 }
 
 export default Users;
