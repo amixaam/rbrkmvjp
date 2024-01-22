16b8d5f09fddf7aa6d66055299c392a9db3168e7
Lxgends <marcitis24@gmail.com>
2024-01-22 10:20:24 +0200

    asdsad

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
+import CSRF from "../../Reusable/csrf"
 import Background from "../../Reusable/Background";
 
 function Login() {
-    function loginHandler(){
+    const navigate = useNavigate();
+    const [Result, setResult] = useState()
+    const [Username, setUsername] = useState();
+    const [Password, setPassword] = useState();
 
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+    const loginHandler = async(e)=> {
+        e.preventDefault();
+        const csrfToken = await CSRF();
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/login", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    username: {Username},
+                    password: {Password},
+                    _token: csrfToken,
+                }),
+            });
+
+            if (!response.ok) {
+                setResult("fail");
+                throw new Error("Login failed");
+            }
+
+            const data = await response.json();
+            setResult("success");
+            sessionStorage.setItem("token", data.token);
+            navigate("/users");
+        } catch (error) {
+            setResult("fail");
+            console.error("Login failed", error);
+        }
     }
 
     return (
@@ -16,19 +60,19 @@ function Login() {
                         <div className="login-input">
                             <div className="input-row">
                                 <h2 className="input-text">Username</h2>
-                                <h3 className="error-text">Error message</h3>
+                                <h3 className="error-text"></h3>
                             </div>
-                            <input type="text" name="name" />
+                            <input type="text" name="name" value={Username} onChange={handleUsernameChange}/>
                         </div>
                         <div className="login-input">
                             <div className="input-row">
-                                <h2 className="input-text">Username</h2>
-                                <h3 className="error-text">Error message</h3>
+                                <h2 className="input-text">Password</h2>
+                                <h3 className="error-text"></h3>
                             </div>
-                            <input type="text" name="name" />
+                            <input type="password" name="name" value={Password} onChange={handlePasswordChange}/>
                         </div>
                     </div>
-                    <button className="login-button" onClick="">
+                    <button className="login-button" onClick={loginHandler}>
                         <h1 className="text">Log in</h1>
                     </button>
                 </div>
