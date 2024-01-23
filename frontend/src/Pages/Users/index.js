768efbf16dedbffa1a6197183b187bc4c0f8ef75
Lxgends <marcitis24@gmail.com>
2024-01-23 10:42:29 +0200

    Finished updated admin
    Bozo

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -59,6 +59,61 @@ function Users() {
           <div className="main-user-title">
             <h1 className="text-primary">Admin interface</h1>
           </div>
+
+          {/*Product List*/}
+          <div className="product-container">
+            <p className="text-secondary">Products</p>
+            <div className="specific-product-titles">
+              <div className="id-title">
+                <p className="small-text">ID</p>
+              </div>
+              <div className="role-title">
+                <p className="small-text">Asignee</p>
+              </div>
+              <div className="other-title">
+                <p className="small-text">Other</p>
+              </div>
+            </div>
+            <div className="specific-user">
+              <div className="id-container">
+                <p>x</p>
+              </div>
+              <div className="role-container">
+                <p>x</p>
+              </div>
+              <div className="other-container">
+                <p>x</p>
+              </div>
+              <div className="edit-container">
+                <button className="user-edit-buttons" onClick={handleEditButtonClick}>
+                  <p className="text-buttons">Edit</p>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="user-buttons">
+            <div className="product-creating-stuff">
+              <button className="user-create-buttons" onClick={handleButtonClick}>
+                <p className="text-buttons">View History</p>
+              </button>
+              <button className="user-create-buttons" onClick={handleButtonClick}>
+                <p className="text-buttons">Add Product</p>
+              </button>
+              <button className="user-create-buttons">
+                <p className="text-buttons">Create Report</p>
+              </button>
+            </div>
+            <div className="product-page-swappers">
+              <button className="user-swap-buttons">
+                <img src={LeftArrow} alt="Previous Button"/>
+              </button>
+              <button className="user-swap-buttons">
+                <img src={RightArrow} alt="Next Button" />
+              </button>
+            </div>
+          </div>
+
+          {/*User List*/}
           <div className="user-list">
             <p className="text-secondary">Users</p>
             <div className="specific-user-titles">
